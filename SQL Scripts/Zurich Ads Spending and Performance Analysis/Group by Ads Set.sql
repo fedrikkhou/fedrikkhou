95c9df08ceeee7c99ae682917a6a3fc3b3f58645
fedrikkhou <145245786+fedrikkhou@users.noreply.github.com>
2024-01-18 22:09:09 +0700

    Zurich Ads Spending and Performance Analysis - SQL Scripts

---
+++ SQL Scripts/Zurich Ads Spending and Performance Analysis/Group by Ads Set.sql
@@ -0,0 +1,19 @@
+-------------------------ADS SET -----------------------------------------------------------------
+SELECT ads_set,
+		SUM(amount_spent) AS Spend,
+		SUM(impressions) AS Impressions,
+		SUM(amount_spent) / SUM(impressions) * 1000 AS CPM,
+		SUM(link_clicks) AS Clicks,
+		CAST(SUM(link_clicks)*1.00 / SUM(impressions)*1.00 as decimal(7,4)) AS CTR,
+		SUM(amount_spent) / SUM(link_clicks) AS CPC,
+		SUM(signup) AS "Sign Up",
+		CAST(SUM(signup)*1.00 / SUM(link_clicks)*1.00 as decimal(7,4))*100 AS "Sign Up Rate",
+		SUM(purchase_zlf + purchase_zlp) AS Purchase,
+		CAST(SUM(purchase_zlf + purchase_zlp)*1.00 / SUM(link_clicks)*1.00 as decimal(7,4)) AS CVR,
+		CASE
+			WHEN SUM(purchase_zlf + purchase_zlp) = 0 THEN null
+			ELSE SUM(amount_spent) / SUM(purchase_zlf + purchase_zlp)
+		END AS CAC
+FROM daily d
+GROUP BY 1
+ORDER BY 2 DESC
---
+++ SQL Scripts/Zurich Ads Spending and Performance Analysis/Group by Region.sql
@@ -0,0 +1,28 @@
+--------------------------REGION----------------------------------------------------------------------------------
+SELECT region,
+		SUM(amount_spent) AS Spend,
+		SUM(impressions) AS Impressions,
+		SUM(amount_spent) / SUM(impressions) * 1000 AS CPM,
+		SUM(link_clicks) AS Clicks,
+		CAST(SUM(link_clicks)*1.00 / SUM(impressions)*1.00 as decimal(7,4)) AS CTR,
+		CASE
+			WHEN SUM(link_clicks) = 0 THEN null
+			ELSE SUM(amount_spent) / SUM(link_clicks)
+		END AS CPC,
+		SUM(signup) AS "Sign Up",		 
+		CASE
+			WHEN SUM(link_clicks) = 0 THEN null
+			ELSE CAST(SUM(signup)*1.00 / SUM(link_clicks)*1.00 as decimal(7,4))*100
+		END AS "Sign Up Rate",  
+		suM(purchase_zlp) + sum(purchase_zlf) AS Purchase,		
+		CASE
+			WHEN SUM(link_clicks) = 0 THEN null
+			ELSE CAST(SUM(purchase_zlf + purchase_zlp)*1.00 / SUM(link_clicks)*1.00 as decimal(7,4))
+		END  AS CVR,
+		CASE
+			WHEN SUM(purchase_zlf + purchase_zlp) = 0 THEN null
+			ELSE SUM(amount_spent) / SUM(purchase_zlf + purchase_zlp)
+		END AS CAC
+FROM region r
+GROUP BY 1
+ORDER BY 2 desc
